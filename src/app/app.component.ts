7f21554e8ac3350589a0b9445d3469bda3b33870
Tirth Jivani <tirthjivani17@gmail.com>
2020-12-08 14:47:25 +0530

    trial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'eKart';
+  title = 'tirth';
 }
